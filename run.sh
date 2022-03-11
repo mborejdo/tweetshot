49e97bc0db874066704a0bc2957bcbc43c11f9a0
mborejdo <mborejdo+github@gmail.com>
2022-03-11 18:29:19 +0100

    add gitignore

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-docker run  --rm -v $(pwd)/data:/github/workspace/data/ --workdir /github/workspace/data/ mborejdo/actions "tweet" "" "1463560516882866179" "xxx.png"
+docker run  --rm -v $(pwd)/data:/github/workspace/data/ --workdir /github/workspace/data/ mborejdo/actions "tweet" "" "1463560516882866179" "xxx"
