1d499618ad07a08261f7d666b4f98650e50fad92
mborejdo <mborejdo+github@gmail.com>
2022-03-17 19:58:30 +0100

    update

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-
-docker run  --rm -v $(pwd)/data:/github/workspace/data/ --workdir /github/workspace/data/ mborejdo/action-wss  "tweet" "" "twitter.html?id=20" "tweet"
+# %7B%22id%22%3A%22mediacoder%22%2C%20%22mode%22%3A%20%22latest%22%7D
+docker run  --rm -v $(pwd)/data:/github/workspace/data/ --workdir /github/workspace/data/ mborejdo/action-wss  "tweet" "" 'twitter.html?data=%7B%22id%22%3A%22mediacoder%22%2C%20%22mode%22%3A%20%22latest%22%7D' "tweet"
