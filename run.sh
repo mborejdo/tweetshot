fbdc3737289b08e2406ac3932c4de607ffb34a4f
mborejdo <mborejdo+github@gmail.com>
2022-03-17 10:49:04 +0100

    updat infile

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-docker run  --rm -v $(pwd)/data:/github/workspace/data/ --workdir /github/workspace/data/ mborejdo/action-wss  "tweet" "" "1463560516882866179" "tweet"
+docker run  --rm -v $(pwd)/data:/github/workspace/data/ --workdir /github/workspace/data/ mborejdo/action-wss  "tweet" "" "twitter.html?id=20" "tweet"
